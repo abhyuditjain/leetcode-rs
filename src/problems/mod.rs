020136933d8f8d2322fd963fd3a8adfdd3135057
Abhyudit Jain <abhyuditjain@gmail.com>
2023-03-17 12:18:58 +0530

    27. Remove Element

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -19,6 +19,7 @@ pub mod problem_23_merge_k_sorted_lists;
 pub mod problem_24_swap_nodes_in_pairs;
 pub mod problem_25_reverse_nodes_in_k_group;
 pub mod problem_26_remove_duplicates_from_sorted_array;
+pub mod problem_27_remove_element;
 pub mod problem_2_add_two_numbers;
 pub mod problem_3_longest_substring_without_repeating_characters;
 pub mod problem_4_median_of_two_sorted_arrays;
