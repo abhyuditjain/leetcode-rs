2120a25c613a10ff87da1452a55244ab3b491b15
Abhyudit Jain <abhyuditjain@gmail.com>
2023-04-04 20:26:39 +0530

    107. Binary Tree Level Order Traversal II

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -5,6 +5,7 @@ pub mod problem_103_binary_tree_zigzag_level_order_traversal;
 pub mod problem_104_maximum_depth_binary_tree;
 pub mod problem_105_construct_binary_tree_from_preorder_and_inorder_traversal;
 pub mod problem_106_construct_binary_tree_from_inorder_and_postorder_traversal;
+pub mod problem_107_binary_tree_level_order_traversal_ii;
 pub mod problem_10_regular_expression_matching;
 pub mod problem_11_container_with_most_water;
 pub mod problem_129_sum_root_to_leaf_numbers;
---
+++ src/problems/problem_107_binary_tree_level_order_traversal_ii.rs
@@ -0,0 +1,80 @@
+//! 107. Binary Tree Level Order Traversal II
+//!
+//! Medium
+//!
+//! Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+//!
+//! Example 1:
+//! Input: root = [3,9,20,null,null,15,7]
+//! Output: [[15,7],[9,20],[3]]
+//!
+//! Example 2:
+//! Input: root = [1]
+//! Output: [[1]]
+//!
+//! Example 3:
+//! Input: root = []
+//! Output: []
+//!
+//! Constraints:
+//! The number of nodes in the tree is in the range [0, 2000].
+//! -1000 <= Node.val <= 1000
+
+use std::{cell::RefCell, collections::VecDeque, rc::Rc};
+
+use crate::utils::treenode::TreeNode;
+
+pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+    let mut queue = VecDeque::new();
+
+    if let Some(node) = root {
+        queue.push_back(node);
+    }
+
+    let mut levels = vec![];
+
+    while !queue.is_empty() {
+        let mut level = vec![];
+
+        for _ in 0..queue.len() {
+            if let Some(node) = queue.pop_front() {
+                level.push(node.borrow().val);
+
+                if let Some(left) = node.borrow_mut().left.take() {
+                    queue.push_back(left);
+                }
+
+                if let Some(right) = node.borrow_mut().right.take() {
+                    queue.push_back(right);
+                }
+            }
+        }
+
+        levels.push(level)
+    }
+
+    levels.into_iter().rev().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_level_order_1() {
+        let root = TreeNode::from_arr(&[Some(3), Some(9), Some(20), None, None, Some(15), Some(7)]);
+        assert_eq!(level_order(root), vec![vec![15, 7], vec![9, 20], vec![3]]);
+    }
+
+    #[test]
+    fn test_level_order_2() {
+        let root = TreeNode::from_arr(&[Some(1)]);
+        assert_eq!(level_order(root), vec![vec![1]]);
+    }
+
+    #[test]
+    fn test_level_order_3() {
+        let root = TreeNode::from_arr(&[]);
+        assert_eq!(level_order(root), vec![vec![]; 0]);
+    }
+}
