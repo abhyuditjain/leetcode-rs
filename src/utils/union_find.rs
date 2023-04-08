86c4ec286af792e4777f266185c5fbf15af97b26
Abhyudit Jain <abhyuditjain@gmail.com>
2023-04-08 10:32:05 +0530

    added tests for union find

--- src/utils/union_find.rs
+++ src/utils/union_find.rs
@@ -45,3 +45,99 @@ impl UnionFind {
         self.find(a) == self.find(b)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_union() {
+        let mut uf = UnionFind::new(5);
+
+        assert!(!uf.union(0, 0));
+        assert!(!uf.union(1, 1));
+        assert!(!uf.union(2, 2));
+        assert!(!uf.union(3, 3));
+        assert!(!uf.union(4, 4));
+
+        assert!(uf.union(0, 1));
+        assert!(uf.union(0, 2));
+        assert!(uf.union(0, 3));
+        assert!(uf.union(0, 4));
+
+        assert!(!uf.union(1, 0));
+        assert!(!uf.union(1, 2));
+        assert!(!uf.union(1, 3));
+        assert!(!uf.union(1, 4));
+
+        assert!(!uf.union(2, 0));
+        assert!(!uf.union(2, 1));
+        assert!(!uf.union(2, 3));
+        assert!(!uf.union(2, 4));
+
+        assert!(!uf.union(3, 0));
+        assert!(!uf.union(3, 1));
+        assert!(!uf.union(3, 2));
+        assert!(!uf.union(3, 4));
+    }
+
+    #[test]
+    fn test_find() {
+        let mut uf = UnionFind::new(5);
+
+        // Initially every node is its own parent
+        assert_eq!(uf.find(0), 0);
+        assert_eq!(uf.find(1), 1);
+        assert_eq!(uf.find(2), 2);
+        assert_eq!(uf.find(3), 3);
+        assert_eq!(uf.find(4), 4);
+
+        // Since ranks of 0 and 1 are same (=1), 0 becomes the parent of 1
+        // Rank of 0 becomes 2
+        uf.union(0, 1);
+        assert_eq!(uf.find(0), 0);
+        assert_eq!(uf.find(1), 0);
+
+        // Since ranks of 2 and 3 are same (=1), 2 becomes the parent of 3
+        // Rank of 2 becomes 2
+        uf.union(2, 3);
+        assert_eq!(uf.find(2), 2);
+        assert_eq!(uf.find(3), 2);
+
+        // Since rank of 2 = 2 and rank of 4 = 1, 2 becomes the parent of 4
+        // Rank of 2 becomes 3
+        uf.union(2, 4);
+        assert_eq!(uf.find(4), 2);
+
+        // Since rank of 2 = 3 and rank of 0 = 2, 2 becomes the parent of 0 and all its connected nodes
+        // Rank of 2 becomes 3 + 2 = 5
+        uf.union(0, 2);
+        assert_eq!(uf.find(2), 2);
+        assert_eq!(uf.find(0), 2);
+        assert_eq!(uf.find(1), 2);
+        assert_eq!(uf.find(3), 2);
+        assert_eq!(uf.find(4), 2);
+    }
+
+    #[test]
+    fn test_is_connected() {
+        let mut uf = UnionFind::new(3);
+
+        assert!(uf.is_connected(0, 0));
+        assert!(uf.is_connected(1, 1));
+        assert!(uf.is_connected(2, 2));
+
+        assert!(!uf.is_connected(0, 1));
+        assert!(!uf.is_connected(0, 2));
+        assert!(!uf.is_connected(1, 2));
+
+        uf.union(0, 1);
+        assert!(uf.is_connected(0, 1));
+        assert!(!uf.is_connected(1, 2));
+
+        uf.union(1, 2);
+        assert!(uf.is_connected(0, 1));
+        assert!(uf.is_connected(1, 2));
+        assert!(uf.is_connected(0, 2));
+    }
+}
